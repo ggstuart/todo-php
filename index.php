afc9ecc2efbb3f84dfc0511efa46154e537f5bd4
Graeme Stuart <ggstuart@gmail.com>
2023-07-11 16:51:55 +0100

    loop over static array and insert template

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php
+$todoLists = [
+    'This is a todo-list',
+    'This is another todo-list',
+    'A third todo-list',
+    'list 4',
+    'The fifth todo-list',
+];
 include 'views/index.phtml';
