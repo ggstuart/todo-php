8b242deffbc5d3458d7d3bc0cfff65e7d794d40a
Graeme Stuart <ggstuart@gmail.com>
2023-07-11 17:16:29 +0100

    a script to handle the post request

---
+++ create-todo-list.php
@@ -0,0 +1,4 @@
+<?php
+echo "<h2>Create a new <em>todo-list</em></h2>";
+echo "<p>You asked the server to create a <em>todo-list</em> with the title <strong>'{$_POST['title']}'</strong></p>";
+echo "<p>...the server ignored you.</p>";
